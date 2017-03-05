216481d45236e39e6b5b3bf1a535ef2349f6a38b
iamstd <iamstd@163.com>
2017-03-05 22:46:34 +0800

    add nodemon

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' }, (err, html) => {
-    res.send('lee')
+    res.send('lee2')
   });
 });
 // product-details
